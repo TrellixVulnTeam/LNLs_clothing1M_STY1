835ecb71059d5c0cc2989fe065285c98bbe96c16
thhwangspa <thhwang@spa.hanyang.ac.kr>
2021-04-20 13:21:11 +0900

    add lr scheduler module

---
+++ lr_sceduler.py
@@ -0,0 +1,61 @@
+from mean_teacher import ramps
+import cli
+
+args = cli.args
+
+def adjust_learning_rate_fastswa(optimizer, epoch,  # modified for fastSWA
+                                 step_in_epoch, total_steps_in_epoch):
+    lr = args.lr # max lr ( initial lr )
+    epoch = epoch + step_in_epoch / total_steps_in_epoch
+
+    # LR warm-up to handle large minibatch sizes from
+    # https://arxiv.org/abs/1706.02677
+    # lr = ramps.linear_rampup(epoch, args.lr_rampup) * (args.lr - args.initial_lr) + args.initial_lr # no rampup when doing fastSWA
+
+    # Cosine LR rampdown from
+    # https://arxiv.org/abs/1608.03983 (but one cycle only)
+    if args.cycle_rampdown_epochs:
+        assert args.cycle_rampdown_epochs >= args.epochs
+        if epoch <= args.epochs:
+            lr *= ramps.cosine_rampdown(epoch, args.cycle_rampdown_epochs)
+        else:
+            epoch_ = (args.epochs - args.cycle_interval) + ((epoch - args.epochs) % args.cycle_interval)
+            lr *= ramps.cosine_rampdown(epoch_, args.cycle_rampdown_epochs)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def adjust_learning_rate_cosineannealing(optimizer, epoch,  # cosineannealing (variable : half interval (args.interval) )
+                                         step_in_epoch, total_steps_in_epoch):
+    lr = args.lr
+    epoch = epoch + step_in_epoch / total_steps_in_epoch
+
+    # LR warm-up to handle large minibatch sizes from
+    # https://arxiv.org/abs/1706.02677
+    # lr = ramps.linear_rampup(epoch, args.lr_rampup) * (args.lr - args.initial_lr) + args.initial_lr
+
+    # Cosine LR rampdown from
+    # https://arxiv.org/abs/1608.03983 (but one cycle only)
+    if args.interval:
+        # assert args.lr_rampdown_epochs >= args.max_epochs_per_filtering
+        lr *= ramps.cosine_rampdown(epoch, args.interval)
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def adjust_learning_rate(optimizer, epoch, # original code
+                         step_in_epoch, total_steps_in_epoch):
+    lr = args.lr
+    epoch = epoch + step_in_epoch / total_steps_in_epoch
+
+    # LR warm-up to handle large minibatch sizes from
+    # https://arxiv.org/abs/1706.02677
+    lr = ramps.linear_rampup(epoch, args.lr_rampup) * (args.lr - args.initial_lr) + args.initial_lr
+
+    # Cosine LR rampdown from
+    # https://arxiv.org/abs/1608.03983 (but one cycle only)
+    if args.lr_rampdown_epochs:
+        assert args.lr_rampdown_epochs >= args.max_epochs_per_filtering
+        lr *= ramps.cosine_rampdown(epoch, args.lr_rampdown_epochs)
+
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
