d18a422adf0b6165afcd945a26bfdcd3231192e2
konyul <konyulpark@gmail.com>
2021-05-11 15:52:49 +0900

    clothing1M version

---
+++ warmup_analyze_clothing1M.py
@@ -0,0 +1,248 @@
+import csv
+import os
+import pdb
+from copy import deepcopy
+import random
+from pathlib import Path
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import DataLoader
+
+import datasets
+from mean_teacher import data, losses
+from utils import send_email
+from noise import generate_noise
+import cli
+from train_utils import save_checkpoint, create_model
+from warmup_trainer_clothing1M import Supervised, Filter
+import architectures
+import pickle, time
+
+
+from train_utils import lr_fastswa
+from utils import progress_bar
+
+# set working directory to current folder
+p = Path(__file__).absolute()
+PATH = p.parents[0]
+os.chdir(PATH)
+
+args = cli.args
+use_cuda = torch.cuda.is_available()
+CUDA_VISIBLE_DEVICES=0
+
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+# for checking behavior of clean and noisy samples
+analysis_dict = {'softmax':[],
+                 'softmax_ema':[],
+                #  'clean_labels':[],
+                #  'noisy_labels':[],
+                 'loss_history':[],
+                 'lr':[]}
+
+"""
+################################# Prepare Data ###############################
+"""
+
+print('==> Preparing data..')
+trainset, valset, testset, num_classes =\
+        datasets.__dict__[args.dataset](args.final_run, args.train_size)
+#analysis_dict['clean_labels'].append(deepcopy(trainset.targets)) # num : 45000
+
+# trainset, clean_idxs_train, noisy_idxs_train = generate_noise(args.dataset,  #trainset : noisy 80% clean 20% 
+#                                                               trainset,
+#                                                               args.noise_type,
+#                                                               args.noise_ratio)
+#analysis_dict['noisy_labels'].append(deepcopy(trainset.targets)) # num : 45000
+
+# if args.noisy_validation:
+#     valset, clean_idxs_val, noisy_idxs_val = generate_noise(args.dataset,
+#                                                             valset,
+#                                                             args.noise_type,
+#                                                             args.noise_ratio)
+# transform_test = deepcopy(testset.transform)
+#filtered_trainset = deepcopy(trainset)            #노이즈 이후의 trainset
+# trainset.transform = transform_test
+
+# filtered_trainloader = DataLoader(filtered_trainset,
+#                                   batch_size=args.batch_size,
+#                                   shuffle=True,
+#                                   num_workers=args.workers,              #noisy
+#                                   pin_memory=True)
+trainloader = DataLoader(trainset,
+                         batch_size=args.batch_size,
+                         shuffle=False,
+                         num_workers=args.workers,                       #noisy
+                         pin_memory=True)
+valloader = DataLoader(valset,
+                       batch_size=200,
+                       shuffle=False,
+                       num_workers=args.workers,
+                       pin_memory=True)
+testloader = DataLoader(testset,
+                        batch_size=200,
+                        shuffle=False,
+                        num_workers=args.workers,
+                        pin_memory=True)
+
+# for saving dataset
+data_dict = {#'filtered_trainloader':filtered_trainloader,
+             'trainloader':trainloader,
+             'valloader':valloader,
+             'testloader':testloader,
+            #  'clean_idxs_train':clean_idxs_train,
+            #  'noisy_idxs_train':noisy_idxs_train,
+            #  'clean_labels':analysis_dict['clean_labels'],
+            #  'noisy_labels':analysis_dict['noisy_labels'],
+             'num_classes':num_classes}
+
+"""
+########################### Initialize Model ##################################
+"""
+
+print('==> Building model..')
+model_factory = architectures.__dict__[args.arch]
+import pdb; pdb.set_trace()
+model = model_factory(pretrained=True,num_classes=14)
+
+model = nn.DataParallel(model).cuda()
+
+name_exp = '_'.join([args.arch, args.dataset, args.noise_type,
+                    str(args.noise_ratio), 'lr', str(args.lr), 'lr_min', str(args.lr_min),
+                    'seed', str(args.seed), 'interval', str(args.interval), 'cycles', str(args.cycles)])
+
+tm = time.localtime(time.time())
+string = str(tm.tm_mon)+'.'+str(tm.tm_mday)+'.'+str(tm.tm_hour)
+directory = os.path.join("./results",string,'warmup')
+
+# make directories
+os.makedirs(directory+'/checkpoint', exist_ok=True)
+os.makedirs(directory+'/log', exist_ok=True)
+os.makedirs(directory+'/plot', exist_ok=True)
+os.makedirs(directory+'/analysis', exist_ok=True)
+
+path_log = (directory+'/log/' + name_exp + '.csv')
+path_plot = (directory+'/plot/' + name_exp + '.png')
+path_plot_lr = (directory+'/plot/' + name_exp + '_lr.png')
+path_analysis = (directory+'/analysis/' + name_exp + '.pickle')
+path_dataset = (directory+'/data_dict.pickle')
+with open(path_dataset, 'wb') as fw:
+    pickle.dump(data_dict, fw)
+
+cudnn.benchmark = True
+print('Number of GPUs:', torch.cuda.device_count())
+print('Using CUDA..')
+
+class_criterion = nn.CrossEntropyLoss(reduction='sum', ignore_index=-1)
+if args.consistency_type == 'mse':
+    consistency_criterion = losses.softmax_mse_loss
+elif args.consistency_type == 'kl':
+    consistency_criterion = losses.softmax_kl_loss
+else:
+    assert False, args.consistency_type
+residual_logit_criterion = losses.symmetric_mse_loss
+
+"""
+################################ Main #########################################
+"""
+
+with open(path_log, 'w') as logfile:
+    logwriter = csv.writer(logfile, delimiter=',')
+    # logwriter.writerow(['epoch', 'train loss', 'train acc', 'clean acc', 'noisy acc', 'noisy memorized',
+    #                     'valid loss', 'valid acc',
+    #                     'test acc'])
+    logwriter.writerow(['epoch', 'train loss', 'train acc',
+                        'valid loss', 'valid acc',
+                        'test acc'])
+
+
+# filtering or finding behavior value module
+#f = Filter(trainloader, analysis_dict['clean_labels'], analysis_dict['noisy_labels'], args)
+#f = Filter(trainloader,args)
+
+accuracy = {'train_acc': [],
+            'val_acc': [],
+            'test_acc': []
+            # 'clean_acc': [],
+            # 'noisy_acc': [],
+            #'noisy_memorized':[]}
+            }
+optimizer = optim.SGD(model.parameters(), lr=args.lr,
+                      momentum=args.momentum,
+                      weight_decay=args.decay, nesterov=args.nesterov)
+
+# supervised learning module
+sl = Supervised(model, optimizer, args)
+epoch = 0
+while epoch < args.interval * args.cycles: # for warm-up analysis with full labeled dataset     10 * 10
+
+    # check lr for every epoch
+    analysis_dict['lr'].append(sl.optimizer.param_groups[0]['lr'])
+    #train_loss, train_acc = sl.train(filtered_trainloader, epoch)
+    train_loss, train_acc = sl.train(trainloader, epoch)
+    val_loss, val_acc = sl.validate(valloader)
+    test_acc = sl.test(testloader)
+    accuracy['train_acc'].append(train_acc)
+    accuracy['val_acc'].append(val_acc)
+    accuracy['test_acc'].append(test_acc)
+
+    #softmax, softmax_ema, loss, clean_acc, noisy_acc, noisy_memorized = f.get_behavior(model)
+    #softmax, softmax_ema, loss = f.get_behavior(model)
+    # accuracy['clean_acc'].append(clean_acc)
+    # accuracy['noisy_acc'].append(noisy_acc)
+    # accuracy['noisy_memorized'].append(noisy_memorized)
+    #analysis_dict['softmax'].append(deepcopy(softmax))
+    #analysis_dict['softmax_ema'].append(deepcopy(softmax_ema))
+    #analysis_dict['loss_history'].append(deepcopy(loss))
+
+    epoch += 1
+    # plot
+    plt.figure()
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Accuracy')
+    ax1.plot(range(epoch), accuracy['train_acc'],
+             color='blue', label='Train_Acc')
+    ax1.plot(range(epoch), accuracy['val_acc'],
+             color='orange', label='Valid_Acc')
+    ax1.plot(range(epoch), accuracy['test_acc'],
+             color='green', label='Test_Acc')
+    # ax1.plot(range(epoch), accuracy['clean_acc'],
+    #          color='midnightblue', label='Clean_Acc')
+    # ax1.plot(range(epoch), accuracy['noisy_acc'],
+    #          color='cornflowerblue', label='Noisy_Acc')
+    # ax1.plot(range(epoch), accuracy['noisy_memorized'],
+    #          color='darkred', label='Noisy_Memorized')
+    ax1.legend(loc='lower right')
+    ax1.grid(True)
+
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('LR')
+    ax2.plot(range(epoch), analysis_dict['lr'],
+             color='dimgray', label='LR', linestyle='--')
+    ax2.legend(loc='upper right')
+
+    fig.savefig(path_plot, dpi=300)
+    plt.close()
+
+    # log
+    with open(path_log, 'a') as logfile:
+        logwriter = csv.writer(logfile, delimiter=',')
+        logwriter.writerow([epoch, train_loss, train_acc,
+                            #clean_acc, noisy_acc, noisy_memorized,
+                            val_loss, val_acc,
+                            test_acc])
+
+
+analysis_dict['accuracy'] = accuracy
+with open(path_analysis, 'wb') as fw:
+    pickle.dump(analysis_dict, fw)
---
+++ warmup_trainer_clothing1M.py
@@ -0,0 +1,202 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pdb
+
+from mean_teacher import losses
+from train_utils import get_current_consistency_weight
+from train_utils import update_ema_variables
+from utils import progress_bar
+from train_utils import lr_cosineannealing, lr_clothing1M
+from torch.autograd import Variable
+
+class Supervised():
+
+    def __init__(self, model, optimizer, args):
+
+        self.seed = args.seed
+        self.model = model
+        self.optimizer = optimizer
+
+        # train configuration
+        self.batch_size = args.batch_size
+        self.logit_distance_cost = args.logit_distance_cost
+        self.total_epochs = args.interval * args.cycles
+        self.args = args
+
+    def train(self, trainloader, epoch):
+        # criterion
+        print('\nEpoch: %d/%d'
+              % (epoch + 1, self.total_epochs))
+        class_criterion = nn.CrossEntropyLoss(reduction='sum', ignore_index=-1)
+        residual_logit_criterion = losses.symmetric_mse_loss
+
+        self.model.train()
+
+        running_class_loss = 0
+        running_res_loss = 0
+        running_loss = 0
+        correct = 0
+        total = 0
+    
+        for batch_idx, (inputs1, targets,_) in enumerate(trainloader):
+            
+            lr_cosineannealing(self.optimizer, epoch, batch_idx, len(trainloader))
+            #lr_clothing1M(self.optimizer,epoch)
+            inputs1, targets = inputs1.cuda(), targets.cuda()
+            outputs = self.model(inputs1)
+
+            minibatch_size = len(targets)
+
+            logit1, logit2 = outputs
+            if self.logit_distance_cost >= 0:
+                class_logit, cons_logit = logit1, logit2
+                res_loss = self.logit_distance_cost * residual_logit_criterion(class_logit, cons_logit) / minibatch_size
+            else:
+                class_logit, cons_logit = logit1, logit1
+                res_loss = 0
+            class_loss = class_criterion(class_logit,
+                                         targets) / minibatch_size
+            
+            _, predicted = torch.max(class_logit, 1)
+            total += minibatch_size
+            correct += predicted.eq(targets).cpu().sum().item()
+
+            loss = class_loss + res_loss
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            running_res_loss += res_loss.item()
+            running_class_loss += class_loss.item()
+            running_loss += loss.item()
+
+            progress_bar(batch_idx, len(trainloader),
+                         'Loss: %.3f | ClassLoss = %.3f | LesLoss: %.3f | Acc: %.3f%% (%d/%d) | lr: %.6f'
+                         % (running_loss / (batch_idx + 1),
+                            running_class_loss / (batch_idx + 1),
+                            running_res_loss / (batch_idx + 1),
+                            100. * correct / total, correct, total,
+                            self.optimizer.param_groups[-1]['lr']))
+
+        loss = {'loss': running_loss / (batch_idx + 1),
+                'class_loss': running_class_loss / (batch_idx + 1),
+                'res_loss': running_res_loss / (batch_idx + 1)}
+        train_acc = 100. * correct / total
+
+        return loss['loss'], train_acc
+
+
+    def validate(self, valloader):
+        self.model.eval()
+
+        running_class_loss = 0
+        correct = 0
+        total = 0
+
+        class_criterion = nn.CrossEntropyLoss()
+        with torch.no_grad():
+            #for batch_idx, (inputs, targets) in enumerate(valloader):
+            for batch_idx, (inputs, targets) in enumerate(valloader):
+                inputs, targets = inputs.cuda(), targets.cuda()
+                outputs, _ = self.model(inputs)
+                minibatch_size = len(targets)
+                class_loss = class_criterion(outputs, targets) / minibatch_size
+                running_class_loss += class_loss.item()
+                _, predicted = torch.max(outputs, 1)
+                total += minibatch_size
+                correct += predicted.eq(targets).cpu().sum().item()
+                progress_bar(batch_idx, len(valloader),
+                             '[Student]Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                             % (running_class_loss/(batch_idx+1),
+                                100.*correct/total, correct, total))
+        acc = 100. * correct / total
+        loss = running_class_loss/(batch_idx+1)
+
+        return loss, acc
+
+
+    def test(self, testloader):
+        self.model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            #for batch_idx, (inputs, targets) in enumerate(testloader):
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                inputs, targets = inputs.cuda(), targets.cuda()
+                outputs, _ = self.model(inputs)
+                _, predicted = torch.max(outputs, 1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).cpu().sum().item()
+                progress_bar(batch_idx, len(testloader),
+                             '[Student]Acc: %.3f%% (%d/%d)'
+                             % (100. * correct / total, correct, total))
+        acc = 100. * correct / total
+        return acc
+
+
+class Filter():
+
+    #def __init__(self, trainloader, clean_labels, noisy_labels, args):
+    def __init__(self, trainloader, args):
+        self.trainloader = trainloader
+        #self.trainset = trainloader.dataset
+        #self.num_classes = len(self.trainset.targets)
+        self.softmax_ema = np.zeros(1)
+        self.ema_decay = args.ema_decay
+
+        #self.clean_labels = np.array(clean_labels[0])       #clean labels from trainset 45000
+        
+        #self.noisy_labels = np.array(noisy_labels[0])
+        #self.clean_or_not = self.clean_labels == self.noisy_labels    # 45000 - 36000
+        #self.noisy_or_not = self.clean_labels != self.noisy_labels    # 36000
+        #self.labeled_idxs_history = np.array([], dtype=int)
+
+
+    def get_behavior(self, model, filtering = False):
+
+        softmax, loss = self._get_softmax(model, self.trainloader)
+        if self.softmax_ema.shape[0] == 1:
+            self.softmax_ema = softmax
+        else:
+            self.softmax_ema = self.ema_decay * self.softmax_ema + (1 - self.ema_decay) * softmax
+
+        ensemble_preds = np.argmax(self.softmax_ema, 1)
+        preds = np.argmax(softmax, 1)
+
+        #assert sum(self.clean_or_not) + sum(self.noisy_or_not) == len(preds)
+
+        #clean_acc = 100.*sum(preds[self.clean_or_not]==self.clean_labels[self.clean_or_not])/sum(self.clean_or_not)
+        #noisy_acc = 100.*sum(preds[self.noisy_or_not]==self.clean_labels[self.noisy_or_not])/sum(self.noisy_or_not)
+        #noisy_memorized = 100. * sum(preds[self.noisy_or_not] == self.noisy_labels[self.noisy_or_not]) / sum(self.noisy_or_not)
+
+        #return softmax, self.softmax_ema, loss, clean_acc, noisy_acc, noisy_memorized
+        return softmax, self.softmax_ema, loss
+
+    def _get_softmax(self, model, dataloader):
+        model.eval()
+        with torch.no_grad():
+            class_criterion = nn.CrossEntropyLoss(reduction='none')
+            #for batch_idx, (inputs, targets) in enumerate(dataloader):
+            for batch_idx, (inputs, targets,_) in enumerate(dataloader):
+                inputs, targets = inputs.cuda(), targets.cuda()
+                outputs, _ = model(inputs)
+                class_loss = class_criterion(outputs, targets)
+                if batch_idx == 0:
+                    logits = outputs.cpu().numpy()
+                    loss = class_loss.cpu().numpy()
+                    
+                else:
+                    logits = np.concatenate((logits, outputs.cpu().numpy()), axis=0)
+                    loss = np.concatenate((loss, class_loss.cpu().numpy()), axis=0)
+
+            softmax = self._logit_to_softmax(logits)
+        return softmax, loss
+
+
+    def _logit_to_softmax(self, a):
+        c = np.max(a, 1).reshape(-1, 1)
+        exp_a = np.exp(a - c)
+        sum_exp_a = np.sum(exp_a, 1).reshape(-1, 1)
+        y = exp_a / sum_exp_a
+        return y
